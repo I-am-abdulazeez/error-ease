7fccba0ca7749427ac75d3b9bb8e5c7bc590c4dd
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-01-22 06:49:04 +0100

    settings: user settings vscode

---
+++ src/settings.ts
@@ -0,0 +1,13 @@
+import * as vscode from 'vscode';
+
+import { Translation } from './types';
+
+export function getSelectedLanguage(): keyof Translation {
+  const availableLanguages: Array<keyof Translation> = ["Yoruba", "Pidgin", "SimpleEnglish"];
+  const configuredLanguage = vscode.workspace.getConfiguration("errorEase").get<string>("selectedLanguage");
+  console.log("Selected language from settings:", configuredLanguage);
+
+  return availableLanguages.includes(configuredLanguage as keyof Translation)
+    ? (configuredLanguage as keyof Translation)
+    : "SimpleEnglish";
+}
