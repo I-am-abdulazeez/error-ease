90c181e6a911c36d1181ba8e9340dff368bbeb0c
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-01-22 15:09:22 +0100

    udpate(getLang): update the function to have default value even before the selection is made

--- src/settings.ts
+++ src/settings.ts
@@ -4,10 +4,12 @@ import { Translation } from './types';
 
 export function getSelectedLanguage(): keyof Translation {
   const availableLanguages: Array<keyof Translation> = ["Yoruba", "Pidgin", "SimpleEnglish"];
-  const configuredLanguage = vscode.workspace.getConfiguration("errorEase").get<string>("selectedLanguage");
+  const configuredLanguage = vscode.workspace
+    .getConfiguration("errorEase")
+    .get<string>("language", "SimpleEnglish"); // Default Language is SimpleEnglish
   console.log("Selected language from settings:", configuredLanguage);
 
   return availableLanguages.includes(configuredLanguage as keyof Translation)
     ? (configuredLanguage as keyof Translation)
-    : "SimpleEnglish";
+    : "SimpleEnglish"; // Fallback to SimpleEnglish, if any problem sele
 }
